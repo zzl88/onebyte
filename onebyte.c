e3578ccf50e0913031855b9eb11d4e7f492dee59
Zonglu Zhang <zhangzl6914@gmail.com>
2017-03-11 13:50:27 +0800

    simple memory copy between kernel and user

--- onebyte.c
+++ onebyte.c
@@ -39,12 +39,14 @@ int onebyte_release(struct inode *inode, struct file *filep)
 
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
-  /*please complete the function on your own*/
+  copy_to_user(buf, onebyte_data, 1);
+  return 1;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-  /*please complete the function on your own*/
+  copy_from_user(onebyte_data, buf, 1);
+  return 1;
 }
 
 static int onebyte_init(void)

fd884546f3a57283e0351d1a8362eeb9a6755127
Zonglu Zhang <zhangzl6914@gmail.com>
2017-03-11 13:53:53 +0800

    update file pointer to avoid infinite read

--- onebyte.c
+++ onebyte.c
@@ -40,7 +40,11 @@ int onebyte_release(struct inode *inode, struct file *filep)
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
   copy_to_user(buf, onebyte_data, 1);
-  return 1;
+  if (*f_pos == 0) {
+    *f_pos += 1;
+    return 1;
+  }
+  return 0;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
